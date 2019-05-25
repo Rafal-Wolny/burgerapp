b0f0082900023c987c782cd5eda3c85611fb5cd1
Rafał Wolny <rafwo96@gmail.comgit config --global user.name Rafal-Wolny>
2019-05-25 15:39:52 +0200

    Added controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,19 @@ import CSSModules from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Start adding ingredients!</p>;
+  }
   return (
     <div className={CSSModules.Burger}>
       <BurgerIngredient type="bread-top" />
--- src/components/Burger/BurgerControls/BurgerControls.js
+++ src/components/Burger/BurgerControls/BurgerControls.js
@@ -1,9 +1,26 @@
 import React from "react";
-
-const controls = () => {
+import classes from "./BurgerControls.css";
+import BurgerControl from "./BurgerControl/BurgerControl";
+const controlers = [
+  { label: "Salad", type: "salad" },
+  { label: "Bacon", type: "bacon" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Meat", type: "meat" }
+];
+const controls = props => {
   return (
-    <Aux>
-      <div> </div>
-    </Aux>
+    <div className={classes.BurgerControls}>
+      {controlers.map(ctrl => (
+        <BurgerControl
+          key={ctrl.label}
+          label={ctrl.label}
+          added={() => props.ingredientAdded(ctrl.type)}
+          removed={() => props.ingredientRemove(ctrl.type)}
+          disabled={props.disabled[ctrl.type]}
+        />
+      ))}
+    </div>
   );
 };
+
+export default controls;
